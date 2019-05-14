5ef3a10c356c94385f150f25390f1be5abe3b749
ManjunathMekala <manjunath.mekala1@gmail.com>
2019-05-14 17:31:08 +0530

    empty

--- force-app/main/default/aura/PictureCarousel/PictureCarouselHelper.js
+++
@@ -1,6 +0,0 @@
-({
-    setSlideWidth: function (component) {
-        var slideWidth = component.find("gallery").getElement().offsetWidth;
-        component.set("v.slideWidth", slideWidth);
-    }
-})
--- force-app/main/default/aura/PictureCarousel/PictureCarouselRenderer.js
+++
@@ -1,6 +0,0 @@
-({
-    afterRender: function (component, helper) {
-        this.superAfterRender();
-		helper.setSlideWidth(component, helper);
-    }
-})
